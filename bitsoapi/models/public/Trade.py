3726306e652d3d526dfdec5f315853194b45fddf
oxsoftdev <oxsoftdev@gmail.com>
2017-07-16 21:34:16 -0400

    fix(order)

--- bitsoapi/models/public/Trade.py
+++ bitsoapi/models/public/Trade.py
@@ -10,19 +10,18 @@ class Trade(BaseModel):
         for (param, value) in kwargs.items():
             if param == 'book':
                 setattr(self, 'book', value)
-            elif param == 'tid':
-                setattr(self, 'tid', value)
-            elif param == 'amount':
-                setattr(self, 'amount', Decimal(str(value)))
-            elif param == 'price':
-                setattr(self, 'price', Decimal(str(value)))
-            elif param == 'maker_side':
-                setattr(self, 'maker_side', value)
             elif param == 'created_at':
                 setattr(self, 'created_at', dateutil.parser.parse(value))
+            elif param == 'amount':
+                setattr(self, 'amount', Decimal(str(value)))
+            elif param == 'maker_side':
+                setattr(self, 'maker_side', value)
+            elif param == 'price':
+                setattr(self, 'price', Decimal(str(value)))
+            elif param == 'tid':
+                setattr(self, 'tid', value)
 
     def __repr__(self):
         return "Trade({Trade})".format(
             Trade=self._repr('book', 'tid', 'amount', 'price')
         )
-
